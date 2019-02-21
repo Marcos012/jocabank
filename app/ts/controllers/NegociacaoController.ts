8d2b202b34700feda590eb2a9792c8c9f213f044
marcos012 <marcospaulo.ad@gmail.com>
2019-02-20 21:12:41 -0300

    Cria view de negociações

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,11 +3,13 @@ class NegociacaoController {
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
         this._inputData = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event) {
@@ -18,11 +20,6 @@ class NegociacaoController {
             parseFloat(this._inputValor.value));
 
         this._negociacoes.adiciona(negociacao);
-
-        this._negociacoes.paraArray().forEach(negociacao => {
-            console.log(negociacao.data);
-            console.log(negociacao.quantidade);
-            console.log(negociacao.valor);
-        });
+        this._negociacoesView.update(this._negociacoes);
     }
 }
